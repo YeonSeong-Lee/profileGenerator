73b20db81bd10c95d334867508eca9841c42ca7b
yeonseong <yeonseong.dev@gmail.com>
2022-09-18 02:36:43 +0900

    FEAT: 자식 컴포넌트로 분리

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,10 @@
 <script>
+	import ImageGenerator from "./ImageGenerator.svelte";
 	let name;
 </script>
 
 <main>
-	{#if name}
-		<h1>{name}</h1>
-	{:else}
-		<h1>defalut image</h1>
-	{/if}
+	<ImageGenerator name={name}/>
 	<p>make your team's profile</p>
 	<input type='text' bind:value={name} />
 </main>
@@ -18,18 +15,5 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
---
+++ src/ImageGenerator.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let name;
+</script>
+
+<div>
+	{#if name}
+		<h1>{name}</h1>
+	{:else}
+		<h1>defalut image</h1>
+{/if}
+</div>
+
+<style>
+	h1 {
+			color: #ff3e00;
+			text-transform: uppercase;
+			font-size: 4em;
+			font-weight: 100;
+		}
+
+		@media (min-width: 640px) {
+			div {
+				max-width: none;
+			}
+		}
+</style>
