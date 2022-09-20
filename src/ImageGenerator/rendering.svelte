454ab4d6e290df65990972b3ab8baa9e60310d1a
yeonseong <yeonseong.dev@gmail.com>
2022-09-20 20:15:15 +0900

    FEAT: input이 아무것도 없을 때 이미지 지우기

--- src/ImageGenerator/rendering.svelte
+++ src/ImageGenerator/rendering.svelte
@@ -2,13 +2,13 @@
 	export const rendering = (name) => {
 		const canvas = document.getElementById("profileGenerator");
         if (!canvas.getContext)
-		return alert('use other broswer');
-        const ctx = canvas.getContext("2d");
-		ctx.fillStyle = "rgb(200, 0, 0)";
-		ctx.fillRect(10, 10, 50, 50);
-		ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect(30, 30, 50, 50);
-		ctx.font = 'bold 48px serif';
+			return alert('use other broswer');	
+		const ctx = canvas.getContext("2d");
+		if (!name) {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			return ;
+		}
+		ctx.font = 'bold 42px serif';
 		ctx.strokeText(`${name}`, 50, 100);
 	}
 </script>
