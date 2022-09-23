d43c0430595e27096b0a923fcc0d1d9d6b9081ae
yeonseong <yeonseong.dev@gmail.com>
2022-09-24 02:24:12 +0900

    FEAT: add randomRGB

--- src/ImageGenerator/_drawLetter.svelte
+++ src/ImageGenerator/_drawLetter.svelte
@@ -1,9 +1,10 @@
 <script context="module">
     import { Letter } from "./_Letter.svelte";
+    import { randomRGB } from "./_randomRGB.svelte";
 
 	export const drawLetter = (name, ctx, canvas) => {
 		const letter = new Letter(name[name.length - 1], canvas.width, canvas.height);
-		ctx.fillStyle = "rgb(200, 0, 0)"
-		ctx.stroke(letter);
+		ctx.fillStyle = randomRGB();
+		ctx.fill(letter);
 	}
 </script>
