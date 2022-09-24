bfdd053195223cd6a4c10bf1d1adfdfdee45fd5a
yeonseong <yeonseong.dev@gmail.com>
2022-09-25 03:36:43 +0900

    TEMP: loop for check

--- src/ImageGenerator/_drawLetter.svelte
+++ src/ImageGenerator/_drawLetter.svelte
@@ -3,8 +3,10 @@
     import { randomColor } from "../utils/_random.svelte";
 
 	export const drawLetter = (name, ctx) => {
-		const letter = new Letter(name[name.length - 1]);
-		ctx.fillStyle = randomColor();
-		ctx.fill(letter);
+		for (let i = 0; i < 42; i++) {
+			const letter = new Letter(name[name.length - 1]);
+			ctx.fillStyle = randomColor();
+			ctx.fill(letter);
+		}
 	}
 </script>
